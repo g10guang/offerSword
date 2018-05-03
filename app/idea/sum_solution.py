89b9bf606ac6e633d13e73257bb1b095dcdc30aa
g10guang <g10guang@gmail.com>
2018-05-03 16:26:22 +0800

    完成 不用使用乘除做加法

---
+++ app/idea/sum_solution.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# author: Xiguang Liu<g10guang@foxmail.com>
+# 2018-05-03 15:55
+# 题目描述：https://www.nowcoder.com/practice/7a0da8fc483247ff8800059e12d7caf1?tpId=13&tqId=11200&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+
+
+class Solution:
+    def Sum_Solution(self, n):
+        """
+        借助 Python 的小 trick
+        """
+        return sum(range(1, n + 1))
+
+
+class Solution2:
+    """
+    循环可以借助递归栈完成，那么问题就转移为如何判断递归是否应该结束
+    """
+
+    def __init__(self):
+        self.result = 0
+
+    def Sum_Solution(self, n):
+        self.result += n
+        # 借助布尔运算的逻辑短路判断递归结束
+        n > 0 and self.Sum_Solution(n - 1)
+        return self.result
+
+
+class Solution3:
+    def __init__(self):
+        self.result = 0
+
+    def Sum_Solution(self, n):
+        """
+        借助异常停止递归
+        """
+        try:
+            t = 1 // n
+        except ZeroDivisionError:
+            return self.result
+        self.result += n
+        self.Sum_Solution(n-1)
+        return self.result
+
