b3690c930d55ee89226b452208e5c3cea70c8614
g10guang <g10guang@gmail.com>
2018-05-04 17:34:12 +0800

    添加另一个解决方案 链表中环的入口结点

--- app/list/entry_node.py
+++ app/list/entry_node.py
@@ -15,6 +15,7 @@ class Solution:
         """
         思路：
         将所有已经访问过的元素加入到集和中，如果访问到某个元素在集和中，那么该元素就是环的入口
+        如果链表环比较长会导致该算法的空间利用率比较底下
         """
         visited = set()
         p = pHead
@@ -23,3 +24,39 @@ class Solution:
             p = p.next
         return p
 
+
+class Solution2:
+    def EntryNodeOfLoop(self, pHead):
+        """
+        思路：
+        1) 使用双指针追及方法先找到环中的一个结点 x
+        2) 顺着环走计算环的长度 l
+        3) p, q = pHead, pHead
+            i) p 先走 l 步
+            ii) p q 同步走
+            当 p == q 时，p 所指向的结点就是入口结点
+        """
+        if pHead.next is None:
+            return None
+        p, q = pHead.next, pHead
+        while p and q and p != q:
+            p = p.next
+            if p.next:
+                p = p.next.next
+            else:
+                return None
+            q = q.next
+        if not p or not q:
+            return None
+        p = p.next
+        l = 1
+        while p != q:
+            l += 1
+            p = p.next
+        p, q = pHead, pHead
+        for _ in range(l):
+            p = p.next
+        while p != q:
+            p = p.next
+            q = q.next
+        return p
