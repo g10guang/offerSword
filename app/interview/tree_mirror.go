5ea60dbb50d1fb269a7a9ed7db2ccbdfc699abd9
liuxiguang <liuxiguang@bytedance.com>
2022-02-15 21:52:21 +0800

    添加二叉树镜像go解法

---
+++ app/interview/tree_mirror.go
@@ -0,0 +1,25 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param pRoot TreeNode类
+ * @return TreeNode类
+ */
+//  https://www.nowcoder.com/practice/a9d0ecbacef9410ca97463e4a5c83be7?tpId=13&tqId=1374963&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+func Mirror(pRoot *TreeNode) *TreeNode {
+	if pRoot == nil {
+		return nil
+	}
+	pRoot.Left, pRoot.Right = pRoot.Right, pRoot.Left
+	Mirror(pRoot.Left)
+	Mirror(pRoot.Right)
+	return pRoot
+}
