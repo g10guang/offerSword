80b9c677a91ea153322e1cd7d8f3996ccb215145
liuxiguang <liuxiguang@bytedance.com>
2022-04-05 00:22:25 +0800

    添加数组中连续子数组和go解法，滑动窗口
    Change-Id: I7d2be7d021a871c89aff35d5f232911851ec782d

--- app/migrate/continuous_sequence.go
+++ app/migrate/continuous_sequence.go
@@ -7,7 +7,7 @@ package main
  * @return int整型二维数组
  */
 // https://www.nowcoder.com/practice/c451a3fd84b64cb19485dad758a55ebe?tpId=13&tqId=11194&rp=1&ru=%2Fta%2Fcoding-interviews&qru=%2Fta%2Fcoding-interviews%2Fquestion-ranking
-func FindContinuousSequence(sum int) [][]int {
+func FindContinuousSequence2(sum int) [][]int {
 	var result [][]int
 	for i := sum/2 + 1; i >= 2; i-- {
 		n := sum / i
@@ -25,6 +25,35 @@ func FindContinuousSequence(sum int) [][]int {
 				t = append(t, k)
 			}
 			result = append(result, t)
+		}
+	}
+	return result
+}
+
+// FindContinuousSequence 滑动窗口解法，看到题解实践下
+func FindContinuousSequence(sum int) [][]int {
+	var result [][]int
+	left, right := 1, 2
+	acc := 3
+	var t []int
+	t = append(t, 1, 2)
+	// left、right 都只会往右边滑动
+	for left <= right && right <= (sum/2+1) {
+		if acc > sum {
+			acc -= left
+			t = t[1:]
+			left++
+		} else if acc < sum {
+			right++
+			acc += right
+			t = append(t, right)
+		} else {
+			c := make([]int, len(t))
+			copy(c, t)
+			result = append(result, c)
+			acc -= left
+			t = t[1:]
+			left++
 		}
 	}
 	return result
