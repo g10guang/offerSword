9978c7639adb9a1d11c544f3c7dcb11ef295c5b8
Liu XiGuang <g10guang@gmail.com>
2025-08-26 13:31:20 +0800

    Update greatest_num.go

--- app/time/greatest_num.go
+++ app/time/greatest_num.go
@@ -6,24 +6,60 @@ package main
  * @return int整型
  */
 func FindGreatestSumOfSubArray(array []int) int {
+    // 动态规划
+    if len(array) == 0 {
+        return 0
+    }
+
+    lastDp := array[0]
+    m := lastDp
+    for i := 1; i < len(array); i++ {
+        v := array[i]
+        currentDp := max(lastDp+v, v)
+
+        if currentDp > m {
+            m = currentDp
+        }
+
+        lastDp = currentDp
+    }
+
+    return m
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a 
+    }
+
+    return b
+}
+
+
+func FindGreatestSumOfSubArray2(array []int) int {
+    // 贪心算法
+    if len(array) == 0 {
+        return 0
+    }
+
 	max := array[0]
-	acc := max
-	for _, v := range array[1:] {
-		if acc > max {
-			max = acc
-		}
-		if acc < 0 {
-			acc = v
-			continue
-		}
-		if acc+v > 0 {
-			acc += v
-		} else {
-			acc = 0
-		}
-	}
-	if acc > max {
-		max = acc
-	}
-	return max
+    acc := max
+    for _, v := range array[1:] {
+        if sum := acc + v; sum > 0 {
+            acc = sum
+        } else {
+            acc = v
+        }
+
+        if v > acc {
+            acc = v
+        }
+
+        if acc > max {
+            max = acc
+        }
+    }
+    
+
+    return max
 }
